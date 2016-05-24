89a4818e3d06af57d0e6f4f7e862cb1f2b8a0521
Ederson de Lima <ederson.lima@locaweb.com.br>
2016-05-24 15:45:05 -0300

    refactoring

--- lib/fog/radosgw/requests/provisioning/delete_key.rb
+++ lib/fog/radosgw/requests/provisioning/delete_key.rb
@@ -14,20 +14,24 @@ module Fog
           }
 
           begin
-            response = Excon.delete("#{@scheme}://#{@host}/#{path}#{query}",
-                                 :headers => signed_headers(params))
-            if !response.body.empty?
-              case response.headers['Content-Type']
-              when 'application/json'
-                response.body = Fog::JSON.decode(response.body)
-              end
-            end
+            response = Excon.delete("#{@scheme}://#{@host}/#{path}#{query}", :headers => signed_headers(params))
+            parse_response(response) unless response.body.empty?
             response
           rescue Excon::Errors::NotFound => e
             raise Fog::Radosgw::Provisioning::KeyNotFound.new
           rescue Excon::Errors::BadRequest => e
             raise Fog::Radosgw::Provisioning::ServiceUnavailable.new
           end
+        end
+
+        private
+
+        def parse_response(response)
+          response.body = Fog::JSON.decode(response.body) if application_json?(response)
+        end
+
+        def application_json?(response)
+          response.headers['Content-Type'] == 'application/json'
         end
       end
     end
